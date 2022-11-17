b438be409857f5763b36d821e4003415a946403f
donghee1009 <116254881+donghee1009@users.noreply.github.com>
2022-11-17 14:11:02 +0900

    Update mainPage.php

--- phpProject/mainPage.php
+++ phpProject/mainPage.php
@@ -1,6 +1,51 @@
-<?php
-
-class mainPage
-{
-}
-
+<!doctype html>
+<html lang="ko">
+<head>
+	<meta charse = utf-8">
+	<title></title>
+	<style>
+	#Dicon{
+	height : 100%;
+	algin : center;
+	float : left;
+	position : absolute;
+	}
+	header{
+	padding : 10px 0;
+	width : 100%;
+	height : 100%;
+	border : 1px solid black;
+	align : center;
+	text-align : center;
+	}
+	nav{
+	width: 100%;
+	margin: 0px;
+	padding : 15px 0;
+	border : 1px solid black;
+	margin : 0px;
+	}
+	#nv{
+	width : 32%;
+	height : 100%;
+	border : 1px solid black;
+	display : inline-block;
+	margin-left: 5px;
+	}
+	#navText{
+	text-align: center;
+	}
+	</style>
+</head>
+<body>
+<header>
+<div id="Dicon"><img src="dicon.jpg"></div>
+<h1>DMU 맛집</h1>
+</header>
+<nav>
+<section id="nv"><p id ="navText">맛집 리스트</p></section>
+<section id="nv"><p id ="navText">리뷰</p></section>
+<section id="nv"><p id ="navText">맛집제보</p></section>
+</nav>
+</body>
+</html>
