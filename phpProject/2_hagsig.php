c62d747b6956e9c043a70a88e4733f069634b4af
Chaebin <103413890+dalnam@users.noreply.github.com>
2022-12-01 16:49:15 +0900

    Update 2_hagsig.php

--- phpProject/2_hagsig.php
+++ phpProject/2_hagsig.php
@@ -6,5 +6,11 @@ include ("headerfile.php");
 <head>
 </head>
 <body>
+<br>
+<div style = "text-align: center;">
+    <iframe src="https://www.dongyang.ac.kr/dongyang/130/subview.do" 
+     width=1200 height=1650 scrolling=no frameBorder=0>
+</iframe>
+</div>
 </body>
 </html>
