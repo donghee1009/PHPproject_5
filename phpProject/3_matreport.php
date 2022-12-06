671423cee30884263f414c28f26a18119027564e
donghee1009 <116254881+donghee1009@users.noreply.github.com>
2022-12-06 18:19:39 +0900

    3,4 upload

--- phpProject/3_matreport.php
+++ phpProject/3_matreport.php
@@ -4,7 +4,99 @@ include ("headerfile.php");
 <!doctype html>
 <html lang="ko">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        #center{
+            width: 1080px;
+            height: 1080px;
+            margin: auto;
+        }
+        #ptext{
+            font-size: 20px;
+            margin-bottom: 5px;
+        }
+        #in{
+            width: 250px;
+            height: 20px;
+            background-color: #e2e2e2;
+        }
+        .star {
+            position: relative;
+            font-size: 2rem;
+            color: #ddd;
+            }
+        .star input {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            left: 0;
+            opacity: 0;
+            cursor: pointer;
+            }
+        .star span {
+            width: 0;
+            position: absolute; 
+            left: 0;
+            color: red;
+            overflow: hidden;
+            pointer-events: none;
+        }
+        #sel{
+            font-size: 18px;
+            height: 30px;
+        }
+        #revbox{
+            width: 480px;
+            height: 160px;
+        }
+        #filebox{
+            width: 500px;
+            height: 800px;
+            display: inline-block;
+            float: left;
+        }
+    </style>
 </head>
 <body>
+    <div id ="center">
+        <form>
+            <p id="ptext">맛집 이름</p>
+            <input id="in" type="text" name="name">
+            <p id="ptext">음식 분야</p>
+            <select id="sel">
+                <option value="kr">한식</option>
+                <option value="jp">일식</option>
+                <option value="ch">중식</option>
+                <option value="bev">커피/음료</option>
+                <option value="ff">패스트푸드</option>
+                <option value="bar">술집</option>
+                <option value="etc">기타</option>
+            </select><br>
+            <p id="ptext" style="display: inline;">개인적인 평점 및 리뷰</p>
+            <span class="star">
+                ★★★★★
+                <span>★★★★★</span>
+                <input type="range" oninput="drawStar(this)" value="1" step="1" min="1" max="5">
+            </span>
+            <script>
+                const drawStar = (target) => {
+                document.querySelector(`.star span`).style.width = `${target.value * 19}%`;
+                }
+            </script><br>
+            <p><textarea id="revbox"></textarea></p>
+            <div id="filebox">
+                <p id="ptext">메뉴판</p>
+                <input type="file" id="Inputimage" name="menufile" accept="image/*">
+                <img style="width: 500px;" id="previewImage" src="https://dummyimage.com/500x500/ffffff/000000.png&text=preview+image">
+            </div>
+            <div id="filebox">
+            <p id="ptext">음식사진</p>
+            <input type="file" id="Inputimage" name="menufile" accept="image/*">
+            <img style="width: 500px;" id="previewImage" src="https://dummyimage.com/500x500/ffffff/000000.png&text=preview+image">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- phpProject/4_matList.php
+++ phpProject/4_matList.php
@@ -11,50 +11,83 @@ include ("headerfile.php");
 	}
     #menuBox{
        margin: auto;
-       width: 1200px;
+       width: 1300px;
        height: 720px;
-       border: 1px solid black;
         }
+   #line1,#line2{
+      width: 1280px;
+      height: 360px;
+      padding-bottom: 5px;
+   }
     #kr,#jp,#ch,#ff,#bar,#blc{
     display: inline-block;
     text-align: center;
-    float: left;
-    }
-    
-    #kr,#jp,#ch,#ff,#bar,#blc{
-       padding: 0px;
-       margin:0px;
-       width: 398px;
-       height: 350px;
-       border: 1px solid blue;
+    width: 398px;
+    height: 350px;
+    border: 2px solid blue;
+    text-align: center;
+    float:left;
     }
     #bev,#etc{
-       display:inline-block;
-       width: 398px;
-       height: 174px;
-       border: 1px solid blue;
+      width: 398px;
+      height: 170px;
+      border: 1px solid blue;
+      padding-bottom: 5px;
     }
     #ltext{
+       color: white;
        font-size: 18px;
        line-height:300px;
+       text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;
+       margin: 0px;
+       padding: 0px;
     }
     #dtext{
        line-height:150px;
+       text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;
+       color:white;
+    }
+    #kr{
+      background-image: url(img/matList/krback.jpg);
+      background-size: cover;
+      background-position: center;
+    }
+    #jp{
+      background-image: url(img/matList/jpback.jpg);
+      background-size: cover;
+      background-position: center;
+    }#ch{
+      background-image: url(img/matList/chback.jpg);
+      background-size: cover;
+      background-position: center;
+    }
+    #ff{
+      background-image: url(img/matList/ffback.jpg);
+      background-size: cover;
+      background-position: center;
+    }#bar{
+      background-image: url(img/matList/barback.jpg);
+      background-size: cover;
+      background-position: center;
+    }#bev{
+      background-image: url(img/matList/bevback.jpg);
+      background-size: cover;
+      background-position: center;
     }
     </style>
 </head>
 <body>
 <div id ="main">
-    <div id ="menuBox">
-    	<section id ="kr"><p id ="ltext">한식</p></section>
-    	<section id ="jp"><p id ="ltext">일식</p></section>
-    	<section id ="ch"><p id ="ltext">중식</p></section>
-    	<div id ="blc">
-        	<div id ="bev"><p id ="dtext">커피/음료</p></div>
-        	<div id ="etc"><p id ="dtext">기타</p></div>
-    	</div>
-    	<section id ="ff"><p id ="ltext">패스트푸드</p></section>
-    	<section id ="bar"><p id ="ltext">술집</p></section>
+   <div id ="menuBox">
+         <a href="5_matMenu.php"><section id ="kr"><p id ="ltext">한식</p></section></a>
+         <a href="5_matMenu.php"><section id ="jp"><p id ="ltext">일식</p></section></a>
+         <a href="5_matMenu.php"><section id ="ch"><p id ="ltext">중식</p></section></a>
+         <div id="blc">
+            <a href="5_matMenu.php"><div id ="bev"><p id ="dtext">커피/음료</p></div></a>
+            <a href="5_matMenu.php"><div id ="etc"><p id ="dtext">기타</p></div></a>
+         </div>
+         <a href="5_matMenu.php"><div id="ff"><p id ="ltext">패스트푸드</p></div></a>
+         <a href="5_matMenu.php"><div id="bar"><p id ="ltext">술집</p></div></a>
 </div>
 </div>
 </body>
